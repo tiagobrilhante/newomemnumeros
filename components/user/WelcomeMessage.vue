32d3db8a8b242c42e5a8ff806e228f5b1b415c93
tiagobrilhante <tiagobrilhantemania@gmail.com>
2025-07-23 16:25:22 -0400

    refactor(WelcomeMessage.vue): adjust spacing and formatting in welcome message template

--- components/user/WelcomeMessage.vue
+++ components/user/WelcomeMessage.vue
@@ -4,8 +4,8 @@
   const appName = config.public.APP_NAME
 </script>
 <template>
-  <h3 v-if="useAuthStore.user" class="text-right mr-5">
-    {{ useAuthStore.user.rank.acronym }} {{ useAuthStore.user.serviceName }}, {{$t('welcomeMessage')}}
+  <h3 v-if="useAuthStore.user" class="text-right mr-5 mt-1">
+    {{ useAuthStore.user.rank.acronym }} {{ useAuthStore.user.serviceName }}, {{ $t('welcomeMessage') }}
     {{ appName }}
   </h3>
 </template>
