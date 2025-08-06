2b9d2f269ce3221c2d9047d5477a47539b4fae00
tiagobrilhante <tiagobrilhantemania@gmail.com>
2025-08-06 08:36:11 -0400

    **chore: remove unused components, services, and stores**
    - Deleted unused Vue components: `FindUserAndLink.vue`, `ShowUserPermissionSetup.vue`.
    - Removed obsolete stores: `permissionSetupStore.ts`, `sectionFunctionUserStore.ts`.
    - Eliminated outdated services: `userService.ts`.
    - Simplified and updated `RoleManagement.vue`, `index.vue` files with modular replacements and refactored usage.

---
+++ app/components/permissions/ListRoles.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card class="border border-solid border-opacity-100 pa-4 mt-4" rounded="xl">
+      <v-card-title>
+        <v-row>
+          <v-col>Lista roles</v-col>
+          <v-col><v-btn prepend-icon="mdi-plus-circle">criar Role</v-btn></v-col>
+        </v-row>
+      </v-card-title>
+    <v-card-text>
+      teste
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+</script>
--- app/components/permissions/RoleManagement.vue
+++ app/components/permissions/RoleManagement.vue
@@ -2,18 +2,8 @@
 </script>
 <template>
 
-  <v-card class="border border-solid border-opacity-100 pa-4 mt-4" rounded="xl">
-    <v-card-title>
-      <v-row>
-        <v-col>Lista roles</v-col>
-        <v-col><v-btn prepend-icon="mdi-plus-circle">criar Role</v-btn></v-col>
-      </v-row>
-    </v-card-title>
-    <v-card-text>
-  Lista aqui
+  <permissions-list-roles />
 
-      <permissions-create-role/>
-    </v-card-text>
-  </v-card>
+  <permissions-create-role />
 
 </template>
